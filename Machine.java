320e10149a791d7de557cabbb67c42ebb501e3dc
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-09-01 18:19:26 -0400

    add addons

--- Machine.java
+++ Machine.java
@@ -18,6 +18,10 @@ public class Machine implements Executer {
         result.setText("tspan_variety0","Milk Choc",0);
         result.setText("tspan_variety1","Cold Choc",0);
         result.setText("tspan_variety2","Hot Choc",0);
+
+        result.setText("tspan_add0","Cream",0);
+        result.setText("tspan_add1","Mallows",0);
+        result.setText("tspan_add2","Sprinks",0);
         
         result.setAudio("startup.mp3",0);
     }
