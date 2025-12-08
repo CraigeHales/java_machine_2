0da110c494422b8f4d5f570eff7764523cc3daff
CraigeHales <71161286+CraigeHales@users.noreply.github.com>
2025-12-08 15:04:47 -0500

    mach name -> Hot Chocolate Machine

--- Machine.java
+++ Machine.java
@@ -57,7 +57,7 @@ public class Machine implements Executer {
         // which one are we?
         result.setText("tspan_load","T2",0);
         // give the machine a name
-        result.setText("tspan_java_machine","Ice Cold Drinks",0);
+        result.setText("tspan_java_machine","Hot Chocolate Machine",0);
         result.setColor("tspan_java_machine","#3366ff",0);
         // initialize the rectangular selection buttons
         for(int i = 0; i<selection.length; i=i+1){
